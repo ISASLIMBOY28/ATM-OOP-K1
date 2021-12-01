a68b740479547c218694c7a7c02f60d1349e38fb
Jason T <70120519+ISASLIMBOY28@users.noreply.github.com>
2021-12-01 13:10:40 +0700

    V1.03
    Mutasi berjalan

--- rekening.java
+++ rekening.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.sql.*;
 import java.io.IOException;
 public class rekening extends user{
+    private float saldo_tabungan, saldo_deposit;
     
     public void getMutasi(){
     try{
@@ -12,14 +13,13 @@ public class rekening extends user{
     ResultSet set = stat.executeQuery("select * from transaksi where no_rek='"+getNo_rek()+"';");
     System.out.println("Nomor Rekening \t Tanggal Transaksi \t Jumlah Transaksi \t Rekening Tujuan");
     while (set.next()){
-        System.out.println(set.getString("no_rek")+"\t"+set.getString("tgl_transaksi")+"\t"+set.getString("jumlah")+"\t"+set.getString("no_rek_tujuan"));
+        System.out.println(set.getString("no_rek")+"\t"+set.getString("tanggal")+"\t"+set.getString("jumlah")+"\t"+set.getString("no_rek_tujuan"));
     }
-}catch (Exception e){}
+}catch (Exception e){System.out.println("Error: "+e.getMessage());}
 }
     
     public void getSaldo(){
         try{
-
         Class.forName("org.sqlite.JDBC");
     Connection k = DriverManager.getConnection("jdbc:sqlite:D:/Programming/OOP ATM/atm.db");
     Statement stat = k.createStatement();
@@ -28,7 +28,34 @@ public class rekening extends user{
     while (set.next()){
         System.out.println(set.getString("no_rek")+"\t"+"Rp. "+set.getFloat("saldo_tabungan")+"\t Rp. "+set.getFloat("saldo_deposit"));
     }
-}catch (Exception e){}
+}catch (Exception e){System.out.println("Error: "+e.getMessage());}
+    }
+    
+    public void getSaldoTabungan(){
+    try{
+        Class.forName("org.sqlite.JDBC");
+    Connection k = DriverManager.getConnection("jdbc:sqlite:D:/Programming/OOP ATM/atm.db");
+    Statement stat = k.createStatement();
+    ResultSet set = stat.executeQuery("select saldo_tabungan from rekening where username='"+getUsername()+"';");
+    while (set.next()){
+        saldo_tabungan=set.getFloat("saldo_tabungan");
+        System.out.println("Saldo Tabungan anda berjumlah Rp. "+saldo_tabungan);
+    }
+}catch (Exception e){System.out.println("Error: "+e.getMessage());}
+    }
+    
+    public float getSaldoDeposit(){
+    try{
+        Class.forName("org.sqlite.JDBC");
+    Connection k = DriverManager.getConnection("jdbc:sqlite:D:/Programming/OOP ATM/atm.db");
+    Statement stat = k.createStatement();
+    ResultSet set = stat.executeQuery("select saldo_deposit from rekening where username='"+getUsername()+"';");
+    while (set.next()){
+        saldo_deposit=set.getFloat("saldo_deposit");
+        System.out.println("Saldo Tabungan anda berjumlah Rp. "+saldo_deposit);
+    }
+}catch (Exception e){System.out.println("Error: "+e.getMessage());}
+return saldo_deposit;
     }
     
 }
--- transaksi.java
+++ transaksi.java
@@ -26,7 +26,7 @@ public class transaksi extends user{
         if(jumlah>=saldo_tabungan){
         stat.executeUpdate("update rekening set saldo_tabungan=saldo_tabungan-"+jumlah+" where no_rek="+getNo_rek()+";");
         stat.executeUpdate("update rekening set saldo_tabungan=saldo_tabungan+"+jumlah+" where no_rek="+no_rek_tujuan+";");
-        stat.executeUpdate("insert into transaksi values("+getNo_rek()+", 'Transfer Uang', curdate(), "+jumlah+","+no_rek_tujuan+");");
+        stat.executeUpdate("insert into transaksi values("+getNo_rek()+", 'Transfer Uang', date('now'), "+jumlah+",'"+no_rek_tujuan+"');");
         System.out.println("Uang sebesar Rp. "+jumlah);
         System.out.println("Telah dikirimkan ke "+no_rek_tujuan);
     }
@@ -35,7 +35,7 @@ public class transaksi extends user{
     } else{ System.out.println("Nomor rekening tidak terdaftar/salah!");
 }
 }
-}catch (Exception e){}
+}catch (Exception e){System.out.println("Error: "+e.getMessage());}
     }
     
     void TarikTunai(){
@@ -45,9 +45,15 @@ public class transaksi extends user{
         Class.forName("org.sqlite.JDBC");
     Connection k = DriverManager.getConnection("jdbc:sqlite:D:/Programming/OOP ATM/atm.db");
     Statement stat = k.createStatement();
-    stat.executeUpdate("update rekening set saldo_tabungan=saldo_tabungan-"+jumlah+" where no_rek="+getNo_rek()+";");
-    System.out.println("");
-    }catch (Exception e){}
+    ResultSet set = stat.executeQuery("select saldo_tabungan from rekening where no_rek='"+getNo_rek()+"';");
+    while(set.next()){
+    saldo_tabungan = set.getFloat("saldo_tabungan");
+    if (jumlah<=saldo_tabungan){
+    stat.executeUpdate("update rekening set saldo_tabungan=saldo_tabungan-"+jumlah+" where no_rek='"+getNo_rek()+"';");
+    stat.executeUpdate("insert into transaksi values('"+getNo_rek()+"', 'Tarik Tunai', date('now'), "+jumlah+",'"+getNo_rek()+"');");
+} else {System.out.println("Saldo anda tidak mencukupi untuk penarikan tunai!");}
+    }
+    }catch (Exception e){System.out.println("Error: "+e.getMessage());}
     }
     
     void addDeposito(){
@@ -58,7 +64,8 @@ public class transaksi extends user{
     Connection k = DriverManager.getConnection("jdbc:sqlite:D:/Programming/OOP ATM/atm.db");
     Statement stat = k.createStatement();
     stat.executeUpdate("update rekening set saldo_deposit=saldo_deposit+"+jumlah+" where no_rek='"+getNo_rek()+"';");
-    }catch (Exception e){}
+    stat.executeUpdate("insert into transaksi values('"+getNo_rek()+"', 'Setor Deposit', date('now'), "+jumlah+",'"+getNo_rek()+"');");
+    }catch (Exception e){System.out.println("Error: "+e.getMessage());}
     }
     
     void addTabungan(){
@@ -69,6 +76,7 @@ public class transaksi extends user{
     Connection k = DriverManager.getConnection("jdbc:sqlite:D:/Programming/OOP ATM/atm.db");
     Statement stat = k.createStatement();
     stat.executeUpdate("update rekening set saldo_tabungan=saldo_tabungan+"+jumlah+" where no_rek='"+getNo_rek()+"';");
-    }catch (Exception e){}
+    stat.executeUpdate("insert into transaksi values('"+getNo_rek()+"', 'Setor Tabungan', date('now'), "+jumlah+",'"+getNo_rek()+"');");
+    }catch (Exception e){System.out.println("Error: "+e.getMessage());}
     }
 }
